07546523c017a1ed7d63e64ddc6eadd3090583d5
Suraj Patil <suraj.gd89@gmail.com>
2022-02-11 10:45:29 +0530

    test change

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -2,7 +2,7 @@ import './intro.css';
 function Intro() {
     return (
         <section className='intro'>
-            <h1 className='name'>Suraj <span>Patil</span></h1>
+            <h1 className='name'>Suraj <span>Ananda Patil</span></h1>
             <h2 className='designation'>Front End Developer</h2>
             <div className='experience'>8 Year's of Experience</div>
         </section>
